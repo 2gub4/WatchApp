f5bed483b665484bd8728c22d9af2996cd6daa2a
2gub4 <jakub.sosna@o2.pl>
2026-05-21 14:06:46 +0200

    add new endpoints to the repository

--- app/src/main/java/com/js/backendassembly/data/repository/WatchAppRepository.kt
+++ app/src/main/java/com/js/backendassembly/data/repository/WatchAppRepository.kt
@@ -7,6 +7,9 @@ import com.js.backendassembly.data.firebase.MovieFirestore
 import com.js.backendassembly.domain.models.profiles.MovieProfile
 import com.js.backendassembly.data.models.dtos.movies.MovieDetailsDto
 import com.js.backendassembly.data.models.dtos.movies.MoviesPageDto
+import com.js.backendassembly.data.models.dtos.shows.TvSeriesDetailsDto
+import com.js.backendassembly.data.models.dtos.shows.TvSeriesPageDto
+import com.js.backendassembly.domain.models.profiles.TvSeriesProfile
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.withContext
@@ -14,40 +17,106 @@ import kotlinx.coroutines.withContext
 const val CURRENT_USER: String = "test_user"
 
 
-object MoviesRepository {
+object WatchAppRepository {
     const val POSTERS_BASE_URL = "https://image.tmdb.org/t/p/w500"
 
-    suspend fun getApiMovieDetails(movieId: Int): MovieDetailsDto? {
-        return when (val response = TmdbApi.MoviesData.fetchMovieDetails(movieId)) {
-            is TmdbApiResult.OnSuccess -> response.data
-            is TmdbApiResult.OnFailure -> {
-                Log.e("Movie Repository", "Could not recieve MovieDetailsDto", response.error)
-                null
+
+    object Movies {
+
+        suspend fun getApiMovieDetails(movieId: Int): MovieDetailsDto? {
+            return when (val response = TmdbApi.MoviesData.fetchMovieDetails(movieId)) {
+                is TmdbApiResult.OnSuccess -> response.data
+                is TmdbApiResult.OnFailure -> {
+                    Log.e("Movie Repository", "Could not recieve MovieDetailsDto", response.error)
+                    null
+                }
+            }
+        }
+
+        suspend fun getMovieProfile(movieId: Int): MovieProfile? {
+            return withContext(Dispatchers.IO) {
+                val apiMovieDetails = async { getApiMovieDetails(movieId) }
+                val potentialUserRating = async { MovieFirestore.RatingData.getMovieRating(CURRENT_USER, movieId.toString()) }
+                val containingLists = async { MovieFirestore.ListData.getListsContainingMovie(CURRENT_USER, movieId.toString()) }
+                val details = apiMovieDetails.await() ?: return@withContext null
+                MovieProfile(
+                    movieDetails = details,
+                    rating = potentialUserRating.await(),
+                    containingLists = containingLists.await()
+                )
+            }
+        }
+
+        suspend fun getMoviePage(pageNumber: Int, listType: String): MoviesPageDto? {
+            return withContext(Dispatchers.IO) {
+                when (val response = TmdbApi.MoviesData.fetchMoviesPage(listType, pageNumber)) {
+                    is TmdbApiResult.OnSuccess -> response.data
+                    is TmdbApiResult.OnFailure -> {
+                        Log.e("Movie Repository", "Could not recieve MoviesPageDto", response.error)
+                        null
+                    }
+                }
+            }
+        }
+
+        suspend fun searchForMovie(query: String, pageNumber: Int): MoviesPageDto? {
+            return withContext(Dispatchers.IO) {
+                when (val response = TmdbApi.MoviesData.fetchSearchedMovies(query, pageNumber)) {
+                    is TmdbApiResult.OnSuccess -> response.data
+                    is TmdbApiResult.OnFailure -> {
+                        Log.e("Movie Repository", "Could not search movies", response.error)
+                        null
+                    }
+                }
             }
         }
     }
 
-    suspend fun getMovieProfile(movieId: Int): MovieProfile? {
-        return withContext(Dispatchers.IO) {
-            val apiMovieDetails = async { getApiMovieDetails(movieId) }
-            val potentialUserRating = async { MovieFirestore.RatingData.getMovieRating(CURRENT_USER, movieId.toString()) }
-            val containingLists = async { MovieFirestore.ListData.getListsContainingMovie(CURRENT_USER, movieId.toString()) }
-            val details = apiMovieDetails.await() ?: return@withContext null
-            MovieProfile(
-                movieDetails = details,
-                rating = potentialUserRating.await(),
-                containingLists = containingLists.await()
-            )
-        }
-    }
-
-    suspend fun getMoviePage(pageNumber: Int, listType: String): MoviesPageDto? {
-        return withContext(Dispatchers.IO) {
-            when (val response = TmdbApi.MoviesData.fetchMoviesPage(listType, pageNumber)) {
+    object TvSeries {
+        suspend fun getApiTvSeriesDetails(seriesId: Int): TvSeriesDetailsDto? {
+            return when (val response = TmdbApi.TvSeriesData.fetchTvSeriesDetails(seriesId)) {
                 is TmdbApiResult.OnSuccess -> response.data
                 is TmdbApiResult.OnFailure -> {
-                    Log.e("Movie Repository", "Could not recieve MoviesPageDto", response.error)
+                    Log.e("Movie Repository", "Could not recieve MovieDetailsDto", response.error)
                     null
+                }
+            }
+        }
+
+        suspend fun getTvSeriesProfile(seriesId: Int): TvSeriesProfile? {
+            return withContext(Dispatchers.IO) {
+                val apiTvSeriesDetails = async { getApiTvSeriesDetails(seriesId) }
+                val potentialUserRating = async { MovieFirestore.RatingData.getMovieRating(CURRENT_USER, seriesId.toString()) }
+                val containingLists = async { MovieFirestore.ListData.getListsContainingMovie(CURRENT_USER, seriesId.toString()) }
+                val details = apiTvSeriesDetails.await() ?: return@withContext null
+                TvSeriesProfile(
+                    seriesDetails = details,
+                    rating = potentialUserRating.await(),
+                    containingLists = containingLists.await()
+                )
+            }
+        }
+
+        suspend fun getTvSeriesPage(pageNumber: Int, listType: String): TvSeriesPageDto? {
+            return withContext(Dispatchers.IO) {
+                when (val response = TmdbApi.TvSeriesData.fetchTvSeriesPage(listType, pageNumber)) {
+                    is TmdbApiResult.OnSuccess -> response.data
+                    is TmdbApiResult.OnFailure -> {
+                        Log.e("Movie Repository", "Could not recieve MoviesPageDto", response.error)
+                        null
+                    }
+                }
+            }
+        }
+
+        suspend fun searchForTvSeries(query: String, pageNumber: Int): TvSeriesPageDto? {
+            return withContext(Dispatchers.IO) {
+                when (val response = TmdbApi.TvSeriesData.fetchSearchedTvSeries(query, pageNumber)) {
+                    is TmdbApiResult.OnSuccess -> response.data
+                    is TmdbApiResult.OnFailure -> {
+                        Log.e("Movie Repository", "Could not search movies", response.error)
+                        null
+                    }
                 }
             }
         }
